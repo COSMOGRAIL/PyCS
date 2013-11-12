0bc9698e53d0d3a5c421335c35b2d68a487fa5eb
mtewes <mtewes@users.noreply.github.com>
2013-11-12 09:59:30 +0000

    Working on set package

--- pycs/tdc/__init__.py
+++ pycs/tdc/__init__.py
@@ -2,6 +2,7 @@
 This subpackage contains code more or less specific to the Time Delay Challenge (tdc)
 """
 
-__all__ = ["util"]
+__all__ = ["util", "est"]
 
 import util
+import est
--- pycs/tdc/est.py
+++ pycs/tdc/est.py
@@ -1,36 +1,77 @@
 """
-
+Stuff to manipulate time delay estimations from different techniques, specific to the TDC.
 """
 
 import pycs.gen.lc
 import numpy as np
 
 
-class estimate:
+class Estimate:
 	"""
 	Class to hold delay estimates for TDC curves, as obtained by various delay estimators.
 	"""
 	
-	def __init__(self, set="tdc0", rung=0, pair=1, td=0.0, tderr=0.0, ms=0.0):
+	def __init__(self, set="tdc0", rung=0, pair=1, method="None", methodpar="None", td=0.0, tderr=0.0, ms=0.0, confidence=0, timetaken=0.0):
 		"""
+
+		:param set: What set (e.g., "tdc0") the curves are from
+		:param rung: rung
+		:param pair: pair
+		
+		:param method: Name of the method
+		:param methodpar: String containing parameters of this method, or username...
+
+		:param td: time delay point estimate
+		:param tderr: 1sigma error estimate on the time delay
+		:param ms: magnitude shift estimate
+		
+		:param confidence: confidence level. 0 = not estimated, 1 = doubtless, 2 = plausible, ...
+		:param timetaken: seconds it took to get this estimate
 
 		"""
 	
 		self.set = set
 		self.rung = rung
 		self.pair = pair
+		
+		self.method = method
+		self.methodpar = methodpar
+
 		self.td = td
 		self.tderr = tderr
 		self.ms = ms
-
-
+		
+		self.confidence = confidence
+		self.timetaken = timetaken
 
 	def __str__(self):
-		return "Season %s [%i, %i]" % (self.name, self.indices[0], self.indices[-1])
-	
-	
-
-importd3cs(filepath='d3cslog.txt', user='vivien-08/11', maxconflevel=4, writeto=None):	
-
+		return "%s %s (%s rung %i pair %i): %.2f +/- %.2f, conf. %i" % (self.method, self.methodpar, self.set, self.rung, self.pair, self.td, self.tderr, self.confidence)
 		
+
+
+def importfromd3cs(filepath, set="tdc0"):
+	"""
+	Reads a d3cs log file and returns the list of estimates
+	"""
 	
+	logfile = open(filepath, 'r')
+	lines = logfile.readlines()
+	logfile.close()
+	
+	lines = [line.split(',') for line in lines]
+	lines = [[element.strip() for element in line] for line in lines]
+	
+	estimates = [
+		Estimate(set=set,
+			rung = int(line[3]),
+			pair = int(line[4]),
+			method = "D3CS", 
+			methodpar = line[2], 
+			td = float(line[5]), 
+			tderr = float(line[6]), 
+			ms = float(line[7]),  
+			confidence = int(line[9]), 
+			timetaken = float(line[10]))
+		for line in lines]
+
+	return estimates
