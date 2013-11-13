7011fd6ab19b3f5dcc30e87595d1bdd4041506c9
mtewes <mtewes@users.noreply.github.com>
2013-11-13 14:05:30 +0000

    working...

--- pycs/tdc/run.py
+++ pycs/tdc/run.py
@@ -4,6 +4,8 @@ Wrapper stuff to run PyCS on TDC data
 
 import os
 import pycs
+import est
+import copy
 import datetime
 import numpy as np
 import matplotlib.pyplot as plt
@@ -19,6 +21,17 @@ class Run:
 		"""
 			
 		self.iniest = iniest
+		
+		# The output estimate of this run :
+		self.outest = copy.deepcopy(iniest)
+		self.outest.method = ""
+		self.outest.methodpar = ""
+		self.outest.td = 0.0
+		self.outest.tderr = 0.0
+		self.outest.ms = 0.0
+		self.outest.confidence = 0
+		self.outest.timetaken = 0.0
+	
 		self.lca = lca
 		self.lcb = lcb
 		
@@ -72,7 +85,8 @@ class Run:
 				
 	def fitsourcespline(self, sploptfct):
 		self.check()
-		# Adding some simple ML to B (required even just for the magshift !)
+		# Adding some simple ML to B (required for the magshift ...)
+		#pycs.gen.polyml.addtolc(self.lca, nparams=1, autoseasonsgap = 100000.0)
 		pycs.gen.polyml.addtolc(self.lcb, nparams=1, autoseasonsgap = 100000.0)
 
 		# And run a first spline
@@ -80,7 +94,7 @@ class Run:
 		self.log("Fitted sourcespline, delay %.2f" % self.lcb.timeshift)
 		
 		relshift = np.fabs(self.lcb.timeshift - self.iniest.td) / self.iniest.tderr
-		self.log("Relative time shift: %.2f" % relshift)
+		self.log("Time shift: %.2f sigma" % relshift)
 		
 		if relshift > 5.0:
 			self.log("That's too much, I stop")
@@ -91,6 +105,9 @@ class Run:
 		# Visu
 		pycs.gen.lc.display([self.lca, self.lcb], [self.sourcespline], figsize=(18, 6), filename=os.path.join(self.plotdir, "sourcespline.png"), verbose=False)
 		
+		# And we save the residuals, to be used later when drawign simulated curves.
+		pycs.sim.draw.saveresiduals([self.lca, self.lcb], self.sourcespline)
+
 		
 
 	def runobs(self, optfct, n=5):
@@ -106,47 +123,107 @@ class Run:
 		#pycs.sim.draw.multidraw([self.lca, self.lcb], onlycopy=True, n=n, npkl=1, simset="copies", simdir=simdir)
 		
 		self.log("Running %i times on the observations..." % n)
-		lcsclist = []
+		lcscoplist = []
 		for i in range(n):
 			
 			# We make some copies to work with
-			lcac = self.lca.copy()
-			lcbc = self.lcb.copy()
-			lcsc = [lcac, lcbc]
+			lcacop = self.lca.copy()
+			lcbcop = self.lcb.copy()
+			lcscop = [lcacop, lcbcop]
 			
 			# Random initial shifts
-			lcac.shifttime(float(np.random.uniform(low=-self.iniest.tderr, high=self.iniest.tderr, size=1)))
-			lcbc.shifttime(float(np.random.uniform(low=-self.iniest.tderr, high=self.iniest.tderr, size=1)))
+			lcacop.shifttime(float(np.random.uniform(low=-self.iniest.tderr, high=self.iniest.tderr, size=1)))
+			lcbcop.shifttime(float(np.random.uniform(low=-self.iniest.tderr, high=self.iniest.tderr, size=1)))
 			
 			# Randomize order
-			pycs.gen.lc.shuffle(lcsc)
-			
-			# And add it
-			lcsclist.append(lcsc)
-		
-		
-		# We run the optimizer on these copies
-		#optfctouts = pycs.sim.run.applyopt(optfct, lcsclist)
+			pycs.gen.lc.shuffle(lcscop)
+			lcscoplist.append(lcscop)
 		
 		# We run the optimizer and unshuffle
-		for lcsc in lcsclist:
-			optfct(lcsc)
-			pycs.gen.lc.objsort(lcsc, verbose=False)
+		for lcscop in lcscoplist:
+			optfct(lcscop)
+			pycs.gen.lc.objsort(lcscop, verbose=False)
 		
 		# We collect the results
-		self.obsres = np.array([([lcsc[1].timeshift - lcsc[0].timeshift]) for lcsc in lcsclist])
-		self.log("Done! I measured %.2f +/- %.2f (intrinsic)" % (np.median(self.obsres), np.std(self.obsres)))
+		self.obsmesdelays = np.array([([lcscop[1].timeshift - lcscop[0].timeshift]) for lcscop in lcscoplist])
+		self.log("Done! I measured %.2f +/- %.2f (intrinsic)" % (np.median(self.obsmesdelays), np.std(self.obsmesdelays)))
+		
+		self.outest.td = np.median(self.obsmesdelays)
+		self.intrinsicratio = np.std(self.obsmesdelays) / self.iniest.tderr
+		self.log("Intrinsic/initial error ratio: %.2f" % self.intrinsicratio)
+		
+	
+	def runobsplot(self):
+		
+		tdmin = self.iniest.td - 3.0*self.iniest.tderr
+		tdmax = self.iniest.td + 3.0*self.iniest.tderr
+		
+		fig = plt.figure(figsize=(10, 3))
+		fig.subplots_adjust(top=0.95, bottom=0.2)
+		plt.hist(self.obsmesdelays, range=(tdmin, tdmax), bins=200, color="green", lw=0)
+		plt.xlabel("Delay [day]")
+		#ax = plt.gca()
+		plt.figtext(0.15, 0.8, "Intrinsic/initial error ratio: %.2f" % self.intrinsicratio)
+		plt.axvline(self.iniest.td - self.iniest.tderr, color="gray", linestyle="-", zorder=20)
+		plt.axvline(self.iniest.td + self.iniest.tderr, color="gray", linestyle="-", zorder=20)
+		plt.axvline(self.outest.td, color="red", linestyle="-", zorder=20)
+		plt.savefig(os.path.join(self.plotdir, "intrinsic_variance.png"))
+		
 	
 	
-	def runobsshow():
+	def runsim(self, optfct, n=5, plots=True):
+		self.check()
+		
+		self.log("Drawing %i simulations..." % n)
+		
+		timeshiftoriga = self.lca.timeshift # So this is what the single spline fit gave us.
+		timeshiftorigb = self.lcb.timeshift
+		
+		lcssimlist = []
+		for i in range(n):
+			
+			# Set some random "true delays"
+			truetsr = self.iniest.tderr
+			self.lca.timeshift = timeshiftoriga + float(np.random.uniform(low = -truetsr, high = truetsr, size=1))
+			self.lcb.timeshift = timeshiftorigb + float(np.random.uniform(low = -truetsr, high = truetsr, size=1))
+			
+			# Draw them
+			lcssim = pycs.sim.draw.draw([self.lca, self.lcb], self.sourcespline, shotnoise="sigma")
+	
+			if plots:
+				pycs.gen.lc.display(lcssim, [self.sourcespline], figsize=(18,6), filename = os.path.join(self.plotdir, "sim_%i.png" % (i)))
+			
+			# Set some wrong "initial delays" for the analysys, around these "true delays".
+			lcssim[0].shifttime(float(np.random.uniform(low=-self.iniest.tderr, high=self.iniest.tderr, size=1)))
+			lcssim[1].shifttime(float(np.random.uniform(low=-self.iniest.tderr, high=self.iniest.tderr, size=1)))
+			
+			# Randomize order
+			pycs.gen.lc.shuffle(lcssim)
+			lcssimlist.append(lcssim)
+		
+		# We run the optimizer and unshuffle
+		self.log("Running on %i simulations..." % n)
+		for lcssim in lcssimlist:
+			optfct(lcssim)
+			pycs.gen.lc.objsort(lcssim, verbose=False)
+		
+		# We collect the results
+		self.simtruedelays = np.array([([lcssim[1].truetimeshift - lcssim[0].truetimeshift]) for lcssim in lcssimlist])
+		self.simmesdelays = np.array([([lcssim[1].timeshift - lcssim[0].timeshift]) for lcssim in lcssimlist])
+		
+		syserr = np.mean(self.simmesdelays - self.simtruedelays)
+		ranerr = np.std(self.simmesdelays - self.simtruedelays)
+		toterr = np.hypot(syserr, ranerr)
+		
+		self.log("Done! Systematic %.2f day, random %.2f day, total error %.2f day." % (syserr, ranerr, toterr))
+		
+		self.outest.tderr = toterr
+		self.totalratio = toterr / self.iniest.tderr
+		self.log("Total/initial error ratio: %.2f" % self.totalratio)
+			
+
+	def runsimplot(self):	
 		pass
-				
-		
-		
-			
-			
-			
-			
 			
 			
 	
