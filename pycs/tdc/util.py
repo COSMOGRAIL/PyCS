b1fd6c8eb73c2ebc5fd4315416a899a02ed2ad78
mtewes <mtewes@users.noreply.github.com>
2014-06-06 12:54:06 +0000

    Adding a fct to list the available pairs

--- pycs/tdc/util.py
+++ pycs/tdc/util.py
@@ -11,7 +11,16 @@ import pycs.gen.lc
 """
 A list of all pairs that are available in TDC1 v2 (useful, as some pairs have been removed...)
 """
-#tdc1v2pairs = range(1, 1036)
+def listtdc1v2pairs():
+	tdc1v2doubles = np.arange(1, 721)
+	tdc1v2quads = np.array(
+		[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 
+		34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 
+		65, 66, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 95, 
+		96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 
+		122, 123, 124, 125, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 139, 140, 141, 142, 143, 144, 145, 146, 
+		147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158]) # Removed are: 11,15,67,94,126,138
+	return list(np.sort(np.concatenate([tdc1v2doubles, 720+(2*tdc1v2quads-1), 720+(2*tdc1v2quads)])))
 
 
 def tdcfilepath(set, rung, pair, skipset=False):
@@ -19,7 +28,7 @@ def tdcfilepath(set, rung, pair, skipset=False):
 	
 	For tdc1 :
 	rung is in [0, 4]
-	pair is in [1, 1036]
+	pair is in [1, 1036]... but some are missing.
 	
 	"""
 	
