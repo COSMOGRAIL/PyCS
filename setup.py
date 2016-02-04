c3a6778d594a3620206f1d223c4acb6c8d8dde2e
mtewes <malte.tewes@gmail.com>
2016-02-04 12:39:31 +0100

    README was renamed txt to md

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
 	name='PyCS',
 	version='2.0dev',
 	description='Python Curve Shifting ',
-	long_description=open('README.txt').read(),
+	long_description=open('README.md').read(),
 	license='GPLv3',
 	url='http://www.cosmograil.org',
 	packages=["pycs", "pycs.disp", "pycs.gen", "pycs.regdiff", "pycs.sim", "pycs.spl"],
