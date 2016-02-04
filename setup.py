9f1b5fd49c4a9bc7f2849db44a00dc5cb3fc036f
mtewes <malte.tewes@gmail.com>
2016-02-04 11:49:42 +0100

    Checking setup.py for install on readthedocs.org

--- setup.py
+++ setup.py
@@ -7,9 +7,7 @@ setup(
 	version='2.0dev',
 	description='Python Curve Shifting ',
 	long_description=open('README.txt').read(),
-	author='Vivien Bonvin and Malte Tewes',
 	license='GPLv3',
-	author_email='malte.tewes[at]epfl.ch',
 	url='http://www.cosmograil.org',
 	packages=["pycs", "pycs.disp", "pycs.gen", "pycs.regdiff", "pycs.sim", "pycs.spl"],
 	package_data={'pycs.gen': ['epfl.png']}
