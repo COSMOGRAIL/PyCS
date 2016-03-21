1f28dff40b93dfdcb78270b7f12d7929871e8c86
mtewes <malte.tewes@gmail.com>
2016-03-21 13:55:27 +0100

    Fixing uncritical bug that resultet in a deprecation warning

--- pycs/gen/spl.py
+++ pycs/gen/spl.py
@@ -172,8 +172,8 @@ class DataPoints():
 		
 		absstabmagerr = self.calcstabmagerr()
 		
-		gaps = self.jds[1:] - self.jds[:-1]
-		gapindices = np.arange(len(self.jds))[gaps > self.stabgap]
+		gaps = self.jds[1:] - self.jds[:-1] # has a length of len(self.jds) - 1
+		gapindices = np.arange(len(self.jds) - 1)[gaps > self.stabgap] # indices of those gaps that are larger than stabgap
 		
 		for n in range(len(gapindices)):
 			i = gapindices[n]
